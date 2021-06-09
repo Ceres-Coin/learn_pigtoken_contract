70e617a0a85b0e35b9b4c7f1b47be92b5259676d
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-06-09 15:05:37 +0800

    add deploy scripts for dina protocol

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,6 +2,7 @@ const Migrations = artifacts.require("Migrations");
 
 const PigToken = artifacts.require("PigToken");
 
+
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
   deployer.deploy(PigToken); //deploy PigToken
---
+++ migrations/2_deploy.js
@@ -0,0 +1,10 @@
+const Dina = artifacts.require("Dina");
+
+module.exports = async function(deployer, network, accounts) {
+    const contract_owner = accounts[0];
+    const devReceive = accounts[1];
+
+    console.log("contract_owner",contract_owner);
+    console.log("devReceive",devReceive);
+
+}
