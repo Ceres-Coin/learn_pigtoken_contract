11b5f8ef702ac3e7fec6fbf44e295e32977cec2e
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-06-09 13:33:06 +0800

    submit

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,8 @@
 const Migrations = artifacts.require("Migrations");
 
+const PigToken = artifacts.require("PigToken");
+
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  // deployer.deploy(PigToken); //deploy PigToken
 };
